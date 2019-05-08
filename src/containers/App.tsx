a745ab3a69c1ecea1fdb7541567039e6993a4a65
mariamkamel <mariammkamel98@gmail.com>
2019-05-09 00:53:06 +0200

    request added

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -4,6 +4,7 @@ import LandingPage from './LandingPage/LandingPage';
 import LoginPage from './Login/LoginPage';
 import RegistrationPage from './Registration/RegistrationPage';
 import EventPage from './EventPage/EventPage';
+import RequestPage from './RequestPage/RequestPage';
 
 const App: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const App: React.FC = () => {
       <Router>
         <Route path="/" exact component={RegistrationPage} />
         <Route path="/event/:id" exact component={EventPage} />
+        <Route path="/request" exact component={RequestPage} />
       </Router>
     </React.Fragment>
   );
---
+++ src/containers/RequestPage/RequestPage.tsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Card, Button } from 'antd';
+import { Request } from '../../dtos/Request';
+import { Link } from 'react-router-dom';
+import { RequestState } from '../../dtos/RequestState.enum';
+import { RequestService } from '../../Services/request.service';
+const { Meta } = Card;
+
+interface IRequestState {
+  request?: Request;
+}
+export default class RequestPage extends Component<IRequestState> {
+  private _requestService: RequestService;
+
+  constructor(props: any) {
+    super(props);
+    this._requestService = new RequestService();
+  }
+  accept = () => {
+    // await this._requestService.acceptRequest(id);
+    console.log('accept');
+  };
+  reject = () => {
+    // await this._requestService.rejectRequest(id);
+
+    console.log('reject');
+  };
+  async componentDidMount() {}
+  render() {
+    return (
+      <div style={{ margin: '5%' }} className="d-flex align-items-center justify-content-center flex-column">
+        <h1 style={{ font: 'bold' }}>Pending Requests</h1>
+        <Card
+          className="w-50"
+          title={
+            <h2>
+              <a href="/LandingPage">Event Name</a>
+            </h2>
+          }
+        >
+          Event Creator Name: <a href="/LandingPage"> Name</a>
+          <br />
+          <br />
+          <div className="d-flex requestAction align-items-center justify-content-center">
+            <Button style={{ minWidth: '15%', marginRight: '10%', color: 'green' }} onClick={this.accept}>
+              ACCEPT
+              <i className="fas fa-check" />
+            </Button>
+            <Button style={{ minWidth: '15%', color: 'red' }} onClick={this.reject}>
+              REJECT
+              <i className="fas fa-times" />
+            </Button>
+          </div>
+        </Card>
+      </div>
+    );
+  }
+}
