f1f7a51689dfb086e622ead702da2edaad4a0095
mariamkamel <mariammkamel98@gmail.com>
2019-05-10 17:19:54 +0200

    request bugs fixed

--- src/containers/RequestPage/RequestPage.tsx
+++ src/containers/RequestPage/RequestPage.tsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import { Card, Button } from 'antd';
 import { Request } from '../../dtos/Request';
 import { RequestService } from '../../services/request.service';
-
+import { RequestCard } from './components/RequestCard';
 interface IRequestState {
-  request?: Request[];
+  request: Request[];
 }
 export default class RequestPage extends Component<{}, IRequestState> {
   private _requestService: RequestService;
@@ -13,48 +12,19 @@ export default class RequestPage extends Component<{}, IRequestState> {
     super(props);
     this._requestService = new RequestService();
   }
-  accept = () => {
-    // await this._requestService.acceptRequest(id);
-    console.log('accept');
-  };
-  reject = () => {
-    // await this._requestService.rejectRequest(id);
 
-    console.log('reject');
-  };
   async componentDidMount() {
-    const e = await this._requestService.getRequest();
-    await this.setState({ request: e.data });
-    console.log('***********', this.state.request);
+    // const e = await this._requestService.getRequest();
+    // await this.setState({ request: e.data });
+    // console.log('***********', this.state.request);
   }
   render() {
     return (
       <div style={{ margin: '5%' }} className="d-flex align-items-center justify-content-center flex-column">
         <h1 style={{ font: 'bold' }}>Pending Requests</h1>
-        {/* {this.state.request.map(req => ( */}
-        <Card
-          className="w-50"
-          title={
-            <h2>
-              <a href="/event/${req.event.id}">Event Name: </a>
-            </h2>
-          }
-        >
-          Event Creator Name: <a href="/LandingPage"> Name </a>
-          <br />
-          <br />
-          <div className="d-flex requestAction align-items-center justify-content-center">
-            <Button style={{ minWidth: '15%', marginRight: '10%', color: 'green' }} onClick={this.accept}>
-              ACCEPT
-              <i className="fas fa-check" />
-            </Button>
-            <Button style={{ minWidth: '15%', color: 'red' }} onClick={this.reject}>
-              REJECT
-              <i className="fas fa-times" />
-            </Button>
-          </div>
-        </Card>
-        {/* // ))} */}
+        {this.state.request.map(req => (
+          <RequestCard title={req.event.name} value={req.event.creator.fullName} />
+        ))}
       </div>
     );
   }
---
+++ src/containers/RequestPage/components/RequestCard.tsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Card, Button } from 'antd';
+import { Request } from '../../../dtos/Request';
+import { RequestService } from '../../../services/request.service';
+import { RouteComponentProps } from 'react-router';
+
+interface IRequestCardProps {
+  title: string;
+  value: string;
+}
+
+export class RequestCard extends Component<IRequestCardProps, {}> {
+  accept = () => {
+    // await this._requestService.acceptRequest(id);
+    console.log('accept');
+  };
+  reject = () => {
+    // await this._requestService.rejectRequest(id);
+
+    console.log('reject');
+  };
+  render() {
+    return (
+      <Card
+        className="w-50"
+        title={
+          <h2>
+            <a href="/event/${req.event.id}">Event Name:{`${this.props.title}`} </a>
+          </h2>
+        }
+      >
+        Event Creator Name: <a href="/LandingPage"> Name: {`${this.props.value}`} </a>
+        <br />
+        <br />
+        <div className="d-flex requestAction align-items-center justify-content-center">
+          <Button style={{ minWidth: '15%', marginRight: '10%', color: 'green' }} onClick={this.accept}>
+            ACCEPT
+            <i className="fas fa-check" />
+          </Button>
+          <Button style={{ minWidth: '15%', color: 'red' }} onClick={this.reject}>
+            REJECT
+            <i className="fas fa-times" />
+          </Button>
+        </div>
+      </Card>
+    );
+  }
+}
